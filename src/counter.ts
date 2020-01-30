1035471464e512b1ef0373cdb6c6aff99c64085d
laander <la@laander.com>
2020-01-30 09:09:03 +0100

    fix unit tests

--- src/counter.ts
+++ src/counter.ts
@@ -1,24 +1,25 @@
 import { ref, computed, watch } from 'vue';
 
-const counter = ref(1);
-const hasBeenReset = ref(false);
-const isCounterOdd = computed(() => counter.value % 2 > 0);
-const watchForReset = watch(() => {
-  if (counter.value === 0) hasBeenReset.value = true;
-});
-const incrementOrReset = () => {
-  if (counter.value < 10) counter.value++;
-  else counter.value = 0;
+export default () => {
+  const counter = ref(1);
+  const hasBeenReset = ref(false);
+  const isCounterOdd = computed(() => counter.value % 2 > 0);
+  const watchForReset = watch(() => {
+    if (counter.value === 0) hasBeenReset.value = true;
+  });
+  const incrementOrReset = () => {
+    if (counter.value < 10) counter.value++;
+    else counter.value = 0;
+  };
+  const reset = () => {
+    watchForReset();
+    counter.value = 0;
+  };
+  return {
+    counter,
+    isCounterOdd,
+    incrementOrReset,
+    hasBeenReset,
+    reset,
+  };
 };
-const reset = () => {
-  watchForReset();
-  counter.value = 0;
-};
-
-export default () => ({
-  counter,
-  isCounterOdd,
-  incrementOrReset,
-  hasBeenReset,
-  reset,
-});
---
+++ tests/unit/counter.spec.ts
@@ -0,0 +1,24 @@
+import { shallowMount } from '@vue/test-utils';
+import useCounter from '@/counter.ts';
+
+describe('Counter', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+  it('initializes the counter', () => {
+    const { counter } = useCounter();
+    expect(counter.value).toBe(1);
+  });
+  it('can increment the counter', () => {
+    const { counter, incrementOrReset } = useCounter();
+    incrementOrReset();
+    expect(counter.value).toBe(2);
+  });
+  it('knows when counter is odd number', () => {
+    const { isCounterOdd, incrementOrReset } = useCounter();
+    incrementOrReset();
+    expect(isCounterOdd.value).toBe(false);
+    incrementOrReset();
+    expect(isCounterOdd.value).toBe(true);
+  });
+});
