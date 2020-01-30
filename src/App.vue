6ef8e92a7f20d3e9b7fe9506d897ad311747934e
laander <la@laander.com>
2020-01-30 08:43:21 +0100

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div class="app">
+    <h3>{{ welcomeMessage }}</h3>
+    <p>
+      My counter: <strong>{{ counter }}</strong>
+    </p>
+    <p>
+      Is value odd? <strong>{{ isCounterOdd ? 'Yes' : 'No' }}</strong>
+    </p>
+    <p>
+      Has been reset? <strong>{{ hasBeenReset ? 'Yes' : 'No' }}</strong>
+    </p>
+    <p>
+      <button @click="incrementOrReset">Increment</button>
+      <button @click="reset">Reset</button>
+    </p>
   </div>
 </template>
 
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import useCounter from './counter';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const welcomeMessage = ref('Hello there!');
+    onMounted(() => {
+      console.log('Yay, lifecycle hook work too!');
+    });
+    return {
+      ...useCounter(),
+      welcomeMessage,
+    };
+  },
+});
+</script>
+
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/counter.ts
@@ -0,0 +1,24 @@
+import { ref, computed, watch } from 'vue';
+
+const counter = ref(1);
+const hasBeenReset = ref(false);
+const isCounterOdd = computed(() => counter.value % 2 > 0);
+const watchForReset = watch(() => {
+  if (counter.value === 0) hasBeenReset.value = true;
+});
+const incrementOrReset = () => {
+  if (counter.value < 10) counter.value++;
+  else counter.value = 0;
+};
+const reset = () => {
+  watchForReset();
+  counter.value = 0;
+};
+
+export default () => ({
+  counter,
+  isCounterOdd,
+  incrementOrReset,
+  hasBeenReset,
+  reset,
+});
